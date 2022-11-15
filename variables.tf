e82a8fc722a85d59fb3999ba9267637d95e63135
funwithdevops <118200984+funwithdevops@users.noreply.github.com>
2022-11-15 12:36:28 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "function_name" {
+  type = string
+}
+
+variable "alias_name" {
+  type = string
+}
+
+variable "iam_role_arn" {
+  type = string
+}
+
+variable "source_dir" {
+  type = string
+}
+
+variable "function_runtime" {
+  type = string
+}
+
+variable "function_handler" {
+  type = string
+}
